b4c7dce3b871d316e0df9548596a27aa33fe581c
Dominic Egginton <eggintod@coventry.ac.uk>
2020-03-28 10:28:52 +0000

    added new camera page

--- routes/camera.js
+++ routes/camera.js
@@ -20,4 +20,23 @@ router.get('/camera/details/:id', async ctx => {
   } else ctx.redirect('/login')
 })
 
+router.get('/camera/new', async ctx => {
+  if (ctx.session.authenticated === true) {
+    await ctx.render('camera_new')
+  } else ctx.redirect('/login')
+})
+
+router.post('/camera/new', async ctx => {
+  if (ctx.session.authenticated === true) {
+    try {
+      const data = ctx.request.body
+      const cameras = await new Cameras(ctx.db)
+      await cameras.add(data.name, data.address)
+      ctx.redirect('/')
+    } catch (error) {
+      await ctx.render('camera_new', { error: error })
+    }
+  }
+})
+
 module.exports = router
