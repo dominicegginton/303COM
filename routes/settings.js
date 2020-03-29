d409dbd1856a6438321cbc30fed26494f0e43061
Dominic Egginton <eggintod@coventry.ac.uk>
2020-03-29 17:28:39 +0100

    added abaility to update admin password

--- routes/settings.js
+++ routes/settings.js
@@ -3,6 +3,7 @@
 /* IMPORT MODULES */
 const Router = require('@koa/router')
 const Cameras = require('../modules/cameras')
+const Admin = require('../modules/admin')
 
 /* SETUP ROUTER */
 const router = new Router()
@@ -12,6 +13,18 @@ router.get('/settings', async ctx => {
     await new Cameras(ctx.db)
     await ctx.render('settings', { streams: Cameras.streams })
   } else ctx.redirect('/login')
+})
+
+router.post('/settings/password', async ctx => {
+  await new Cameras(ctx.db)
+  const admin = await new Admin(ctx.db)
+  try {
+    const data = ctx.request.body
+    await admin.updatePassword(data.currentPassword, data.newPassword, data.confirmPassword)
+    ctx.redirect('/')
+  } catch (error) {
+    await ctx.render('settings', { streams: Cameras.streams, error: error })
+  }
 })
 
 module.exports = router
