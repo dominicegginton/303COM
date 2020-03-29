bf4c68b8c6f6752b2a590229cffe1f6f2aff0393
Dominic Egginton <eggintod@coventry.ac.uk>
2020-03-29 16:51:04 +0100

    add camera details to settings page

--- routes/settings.js
+++ routes/settings.js
@@ -2,13 +2,15 @@
 
 /* IMPORT MODULES */
 const Router = require('@koa/router')
+const Cameras = require('../modules/cameras')
 
 /* SETUP ROUTER */
 const router = new Router()
 
 router.get('/settings', async ctx => {
   if (ctx.session.authenticated === true) {
-    await ctx.render('settings')
+    await new Cameras(ctx.db)
+    await ctx.render('settings', { streams: Cameras.streams })
   } else ctx.redirect('/login')
 })
 
