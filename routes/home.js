3f21cda7ab481c52f5fd72f60aab355083c14b43
Dominic Egginton <eggintod@coventry.ac.uk>
2020-03-31 18:10:51 +0100

    pass auth status to views

--- routes/home.js
+++ routes/home.js
@@ -2,13 +2,16 @@
 
 /* IMPORT MODULES */
 const Router = require('@koa/router')
+const Cameras = require('../modules/cameras')
 
 /* SETUP ROUTER */
 const router = new Router()
 
 router.get('/', async ctx => {
-  if (ctx.session.authenticated !== true) ctx.redirect('/login')
-  else await ctx.render('home')
+  if (ctx.session.authenticated === true) {
+    await new Cameras(ctx.db)
+    await ctx.render('home', { authenticated: true, streams: Cameras.streams })
+  } else ctx.redirect('/login')
 })
 
 module.exports = router
--- routes/settings.js
+++ routes/settings.js
@@ -11,20 +11,22 @@ const router = new Router()
 router.get('/settings', async ctx => {
   if (ctx.session.authenticated === true) {
     await new Cameras(ctx.db)
-    await ctx.render('settings', { streams: Cameras.streams })
+    await ctx.render('settings', { authenticated: true, streams: Cameras.streams })
   } else ctx.redirect('/login')
 })
 
 router.post('/settings/password', async ctx => {
-  await new Cameras(ctx.db)
-  const admin = await new Admin(ctx.db)
-  try {
-    const data = ctx.request.body
-    await admin.updatePassword(data.currentPassword, data.newPassword, data.confirmPassword)
-    ctx.redirect('/')
-  } catch (error) {
-    await ctx.render('settings', { streams: Cameras.streams, error: error })
-  }
+  if (ctx.session.authenticated === true) {
+    await new Cameras(ctx.db)
+    const admin = await new Admin(ctx.db)
+    try {
+      const data = ctx.request.body
+      await admin.updatePassword(data.currentPassword, data.newPassword, data.confirmPassword)
+      ctx.redirect('/')
+    } catch (error) {
+      await ctx.render('settings', { authenticated: true, streams: Cameras.streams, error: error })
+    }
+  } else ctx.redirect('/login')
 })
 
 module.exports = router
